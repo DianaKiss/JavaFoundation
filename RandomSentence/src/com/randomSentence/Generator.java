cd94d0eee80a4e416bf0b8ba372197907f5bc869
DianaKiss <dana.butcher@gmail.com>
2018-11-13 14:49:49 +0100

    Random sentence generator halfway

---
+++ RandomSentence/src/com/randomSentence/Generator.java
@@ -0,0 +1,15 @@
+package com.randomSentence;
+
+public class Generator {
+
+
+    public static void main(String[] args) {
+
+        SentenceBase sentence = new SentenceBase();
+
+
+        System.out.println(sentence.sentence());
+
+    }
+
+}
---
+++ RandomSentence/src/com/randomSentence/SentenceBase.java
@@ -0,0 +1,20 @@
+package com.randomSentence;
+
+public class SentenceBase {
+    String[] firstWord = {"The best", "My favorite", "The worst", "The most outstanding"};
+    String[] secondWord = {"solution", "thing to do", "thing to think", "thing to say"};
+    String[] thirdWord = {"to hide", "to learn more", "to say nice things", "to behave well"};
+
+    public int getLength(String[] anyArray) {
+
+        int arrayRandom = (int) Math.random() * anyArray.length;
+        return arrayRandom;
+    }
+
+    public String sentence() {
+
+        String sentence = firstWord[getLength(firstWord)] + " " + secondWord[getLength(secondWord)] + " is " + thirdWord[getLength(thirdWord)];
+        return sentence;
+    }
+
+}
