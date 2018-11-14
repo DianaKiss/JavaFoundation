64c1be456f7f72a36685d64640fd27cb4787e787
DianaKiss <dana.butcher@gmail.com>
2018-11-14 10:49:02 +0100

    More basics discovered

---
+++ HelloWorld/src/com/example/arrays/Arrays.java
@@ -0,0 +1,9 @@
+package com.example.arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        String[] myArray = {"pig", "apple", "cat", "cucumber"};
+
+        System.out.println(myArray.length + 100 + " " + myArray[3]);
+    }
+}
