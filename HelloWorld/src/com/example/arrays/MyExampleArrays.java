1b542776d249a4e190cf3f985e1b2b11843d9751
DianaKiss <dana.butcher@gmail.com>
2018-11-14 13:01:52 +0100

    Random commit

--- HelloWorld/src/com/example/arrays/MyExampleArrays.java
+++ HelloWorld/src/com/example/arrays/MyExampleArrays.java
@@ -1,17 +1,23 @@
 package com.example.arrays;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MyExampleArrays {
 
     public static String[] myArray = {"pig", "apple", "cat", "cucumber"};
 
+    public static ArrayList<String> theNicestArrayList = new ArrayList<>();
+
 
     public static void main(String[] args) {
 
 
         System.out.println(myArray.length + 100 + " " + myArray[3]);
-
         System.out.println(Arrays.toString(myArray));
+
+        theNicestArrayList.add("firstElement");
+
+        System.out.println(Arrays.toString(theNicestArrayList.toArray()));
 
     }
 
