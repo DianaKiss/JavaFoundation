6941b537d30231cc1c813bd59ef5dc40b86e19c4
DianaKiss <dana.butcher@gmail.com>
2018-11-15 12:41:15 +0100

    Some more random commit

--- RandomSentence/src/com/randomSentence/Generator.java
+++ RandomSentence/src/com/randomSentence/Generator.java
@@ -6,8 +6,6 @@ public class Generator {
     public static void main(String[] args) {
 
         SentenceBase sentence = new SentenceBase();
-
-
         System.out.println(sentence.sentence());
 
     }
